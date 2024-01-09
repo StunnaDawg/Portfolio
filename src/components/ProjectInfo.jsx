8fa16757a7e2798f53ac02c81fe2b1a0e69a31bb
StunnaDawg <jonsonallen9@gmail.com>
2024-01-08 20:51:05 -0400

    added carosuel

--- src/components/ProjectInfo.jsx
+++ src/components/ProjectInfo.jsx
@@ -1,4 +1,6 @@
 import Section from "./ProjectInfoSection"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from "react-responsive-carousel"
 
 const ProjectInfo = ({
   title,
@@ -39,24 +41,31 @@ const ProjectInfo = ({
               "Lorem ipsum dolor sit amet. In malesuada velit dapibus interdum viverra. Morbi vel ipsum fringilla, aliquam felis at, euismod enim. Fusce elementum at neque vel maximus. Vivamus ut velit tortor. Duis dapibus congue pellentesque. Aenean feugiat vulputate nisi, et blandit leo pulvinar in."
             }
           />
-
-          {/*             
-          <div className=" mb-2">
-            <h1 className="font-bold text-sm mb-3">Description</h1>
-            <div>
-              <p className="text-xs">{projectDescription}</p>
-            </div>
-          </div> */}
-
-          {/* Images */}
           <div className=" flex flex-row justify-center">
             <h1 className="font-bold text-sm mb-3">ScreenShots</h1>
           </div>
 
-          <div className="flex md:flex-row md:justify-center flex-col mb-3">
-            <div className="md:mx-2 my-2">
-              <img className="w-48" src={imageLink1} />
-            </div>
+          <div className="flex flex-col items-center">
+            <Carousel
+              width={200}
+              showArrows
+              autoPlay
+              useKeyboardArrows={true}
+              showThumbs={false}
+              ariaLabel="bg-red"
+              interval={3000}
+              infiniteLoop
+            >
+              <div>
+                <img src={imageLink1} />
+              </div>
+              <div>
+                <img src={imageLink2} />
+              </div>
+              <div>
+                <img src={imageLink3} />
+              </div>
+            </Carousel>
           </div>
         </div>
       </div>
--- src/components/ProjectInfoSection.jsx
+++ src/components/ProjectInfoSection.jsx
@@ -1,8 +1,10 @@
 const Section = ({ title, content }) => {
   return (
     <>
-      <div className="mb-2 text-center mx-11">
+      <div className="flex flex-row justify-center">
         <h1 className="font-bold text-sm mb-3">{title}</h1>
+      </div>
+      <div className="mb-2 text-wrap mx-11 md:px-24">
         <div>
           <p className="text-xs">{content}</p>
         </div>
